b7cd5ca2f5602719c74dba15d52256ee88577905
pttoanprovip <phanthanhtoan248@gmail.com>
2025-06-05 20:48:30 +0700

    add view user about create, update, delete, detail

--- obj/Debug/net8.0/QLNhaSach1.AssemblyInfo.cs
+++ obj/Debug/net8.0/QLNhaSach1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QLNhaSach1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d45626511ad1fff2fa736b2fd451cf38bc9e12b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b1a17dec31244c0d25ba39552aa915f96219daf")]
 [assembly: System.Reflection.AssemblyProductAttribute("QLNhaSach1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QLNhaSach1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
