442624c32530568d426c4d249f718cb6d2b4b1c1
CHuan0806 <buiconghuan62@gmail.com>
2025-06-25 10:56:29 +0700

    PartialView

--- obj/Debug/net8.0/QLNhaSach1.AssemblyInfo.cs
+++ obj/Debug/net8.0/QLNhaSach1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QLNhaSach1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e6fd5cf62d802bbe360f933c56b4031af90d67c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b346f08a2f352618900f0efd36fb488ebc05efd")]
 [assembly: System.Reflection.AssemblyProductAttribute("QLNhaSach1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QLNhaSach1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
